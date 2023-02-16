9dcc8f6d217a247f93ea882aef4b02e8fd8c1875
PythonNut <PythonNut@users.noreply.github.com>
2023-02-16 01:04:49 +0000

    resharder: ignore duplicate file error in webdataset à la openclip

--- resharder.py
+++ resharder.py
@@ -34,6 +34,12 @@ import pyarrow.parquet as pq
 
 from cloudpathlib import CloudPath
 from img2dataset.blurrer import BoundingBoxBlurrer
+from webdataset.tariterators import (
+    base_plus_ext,
+    url_opener,
+    tar_file_expander,
+    valid_sample,
+)
 
 
 Pipe = wds.writer.gopen.Pipe
@@ -286,6 +292,57 @@ class ShardWriter:
     def __exit__(self, *args, **kw):
         """Exit context."""
         self.close()
+
+
+def group_by_keys_nothrow(
+    data, keys=base_plus_ext, lcase=True, suffixes=None, handler=None
+):
+    """Return function over iterator that groups key, value pairs into samples.
+    :param keys: function that splits the key into key and extension (base_plus_ext)
+    :param lcase: convert suffixes to lower case (Default value = True)
+    """
+    current_sample = None
+    for filesample in data:
+        assert isinstance(filesample, dict)
+        fname, value = filesample["fname"], filesample["data"]
+        prefix, suffix = keys(fname)
+        if prefix is None:
+            continue
+        if lcase:
+            suffix = suffix.lower()
+        # FIXME webdataset version throws if suffix in current_sample, but we have a potential for
+        #  this happening in the current LAION400m dataset if a tar ends with same prefix as the next
+        #  begins, rare, but can happen since prefix aren't unique across tar files in that dataset
+        if (
+            current_sample is None
+            or prefix != current_sample["__key__"]
+            or suffix in current_sample
+        ):
+            if valid_sample(current_sample):
+                yield current_sample
+            current_sample = dict(__key__=prefix, __url__=filesample["__url__"])
+        if suffixes is None or suffix in suffixes:
+            current_sample[suffix] = value
+    if valid_sample(current_sample):
+        yield current_sample
+
+
+def tarfile_samples_nothrow(src, handler):
+    # NOTE this is a re-impl of the webdataset impl with group_by_keys that doesn't throw
+    try:
+        streams = url_opener(src, handler=handler)
+        files = tar_file_expander(streams, handler=handler)
+        samples = group_by_keys_nothrow(files, handler=handler)
+
+    except Exception as exn:
+        exn.args = exn.args + (src)
+        handler(exn)
+        return []
+
+    return samples
+
+
+tarfile_to_samples_nothrow = wds.filters.pipelinefilter(tarfile_samples_nothrow)
 
 
 @dataclass(frozen=True, slots=True)
@@ -745,9 +802,14 @@ def copy_worker(
         return True
 
     subset = load_subset(subset_file=subset_file)
-    ds = wds.WebDataset(
-        [str(input_dir / shard_format.format(shard.shard_id)) for shard in task.shards],
-        handler=log_and_continue,
+    ds = wds.DataPipeline(
+        wds.SimpleShardList(
+            [
+                str(input_dir / shard_format.format(shard.shard_id))
+                for shard in task.shards
+            ]
+        ),
+        tarfile_to_samples_nothrow(handler=log_and_continue),
     )
 
     # create shard_name → parquet_name mapping
